ac1b40bc65441c76963ecd73aab4f17adce550d2
KateMassa <catherineorlova27@gmail.com>
2024-04-18 14:17:08 +0200

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectNameFilter } from "../../redux/filters/slice";
+import { selectNameFilter, changeFilter } from "../../redux/filters/slice";
 import { searchContact } from "../../redux/contacts/operations";
+import { selectFilteredContacts } from "../../redux/contacts/selectors";
 import css from "./SearchBox.module.css";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
   const nameFilter = useSelector(selectNameFilter);
   const [searchTerm, setSearchTerm] = useState(nameFilter);
+  // const filteredContacts = useSelector(selectFilteredContacts);
 
   const handleSearch = (event) => {
     const value = event.target.value;
     setSearchTerm(value);
     dispatch(searchContact(value));
+    dispatch(changeFilter(event.target.value));
+
+    // if (filteredContacts.length === 0) {
+    //   setSearchTerm("");
+    // }
   };
 
   return (
