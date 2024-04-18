d2a5a780bf43886c2718e3efaadcd460d5f7c835
KateMassa <catherineorlova27@gmail.com>
2024-04-18 10:20:16 +0200

    update changes
    to fix filter

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DocumentTitle from "../../components/DocumentTitle";
 import ContactList from "../../components/ContactList/ContactList";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import { fetchContacts } from "../../redux/contacts/operations";
-import { selectLoading } from "../../redux/contacts/selectors";
+import { selectIsLoading, selectError } from "../../redux/contacts/selectors";
 import SearchBox from "../../components/SearchBox/SearchBox";
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -17,8 +19,10 @@ export default function ContactsPage() {
   return (
     <>
       <DocumentTitle>Your contacts</DocumentTitle>
+      <ContactForm />
+      <h2>Contacts</h2>
       <SearchBox />
-      <div>{isLoading && "Request in progress..."}</div>
+      <div>{isLoading && !error && <b>Request in progress...</b>}</div>
       <ContactList />
     </>
   );
